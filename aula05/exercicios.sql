77fd1ea14bd940f67d7fbc160cb18e8581219307
Tiago Fialho <tiago.chem@gmail.com>
2024-09-09 15:45:17 -0300

    exercicio aula 05 - finalizado

--- aula05/exercicios.sql
+++ aula05/exercicios.sql
@@ -1,6 +1,33 @@
+-- Fazer uma análise de crescimento mensal e o cálculo de YTD usando CTE
 
-
-
+WITH ReceitasMensais AS (
+    SELECT
+        EXTRACT(YEAR FROM o.order_date) AS ano,
+        EXTRACT(MONTH FROM o.order_date) AS mes,
+        ROUND(SUM((od.unit_price * od.quantity) * (1 - od.discount))) AS receita_mensal
+    FROM orders o
+    INNER JOIN order_details od ON od.order_id = o.order_id
+    GROUP BY
+        EXTRACT(YEAR FROM o.order_date),
+        EXTRACT(MONTH FROM o.order_date)
+),
+ReceitasMensaisAcumuladas AS (
+    SELECT
+        ano,
+        mes,
+        receita_mensal,
+        SUM(receita_mensal) OVER (PARTITION BY ano ORDER BY mes) AS receita_ytd
+    FROM ReceitasMensais
+)
+SELECT
+    ano,
+    mes,
+    receita_mensal,
+    receita_mensal - LAG(receita_mensal) OVER (PARTITION BY ano ORDER BY mes) AS diferenca_mensal,
+    receita_ytd,
+    ROUND((receita_mensal - LAG(receita_mensal) OVER (PARTITION BY ano ORDER BY mes)) * 100 / LAG(receita_mensal) OVER (PARTITION BY ano ORDER BY mes)) AS percentual_mudanca_mensal
+FROM ReceitasMensaisAcumuladas
+ORDER BY ano, mes;
 
 
 
